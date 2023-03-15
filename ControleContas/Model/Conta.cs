14a3ff0cf216737731758a169b6f6ff00032a5a7
rosenclever.gazoni <rosenclever.gazoni@TPCP04LAB1002>
2023-03-15 19:49:11 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContas/Model/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas.Model
+{
+    public class Conta
+    {
+
+        private string _numero;
+        private decimal _saldo;
+
+        //Polimorfismo de sobrecarga
+        public Conta(string numero)
+        {
+            _numero = numero;
+        }
+
+        public Conta(string numero, decimal saldo) 
+        {
+            _saldo = saldo;
+            _numero = numero;
+        }
+
+        public string Numero {
+            get => _numero; 
+            private set => _numero = value; 
+        }
+        public decimal Saldo { 
+            get => _saldo; 
+            private set => _saldo = value; 
+        }
+    }
+}
---
+++ ControleContas/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using ControleContas.Model;
+
+Conta conta = new Conta("1234");
+Conta conta2 = new Conta("4321", 1000.99m);
+Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
